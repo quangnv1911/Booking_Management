fe9b1c15045bad1e0f74dbff465ea92762d5e393
Vương Công Minh <116176389+Kaovuhz@users.noreply.github.com>
2024-07-20 05:54:28 +0700

    add booking

--- Group5_SE1730_BookingManagement/Pages/Hotel/BookingManagement/Index.cshtml.cs
+++ Group5_SE1730_BookingManagement/Pages/Hotel/BookingManagement/Index.cshtml.cs
@@ -6,28 +6,49 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Group5_SE1730_BookingManagement.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Group5_SE1730_BookingManagement.Pages.Hotel.BookingManagement
 {
     public class IndexModel : PageModel
     {
+        private UserManager<Guest> _userManager;
+        private Guest CurrentUser { get; set; }
         private readonly Group5_SE1730_BookingManagement.Models.Group_5_SE1730_BookingManagementContext _context;
 
-        public IndexModel(Group5_SE1730_BookingManagement.Models.Group_5_SE1730_BookingManagementContext context)
+        public IndexModel(Group5_SE1730_BookingManagement.Models.Group_5_SE1730_BookingManagementContext context, UserManager<Guest> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
-        public IList<Booking> Booking { get;set; } = default!;
+        public IList<Booking> Booking { get; set; } = default!;
 
         public async Task OnGetAsync()
         {
+            if (!User.Identity.IsAuthenticated)
+            {
+                Response.Redirect("/Login");
+                return;
+            }
+
+            CurrentUser = await _userManager.GetUserAsync(User);
+
+            if (CurrentUser == null)
+            {
+                // Handle case where user is not found
+                Response.Redirect("/Login");
+                return;
+            }
+
             if (_context.Bookings != null)
             {
                 Booking = await _context.Bookings
-                .Include(b => b.Guest)
-                .Include(b => b.Homestay)
-                .Include(b => b.Room).ToListAsync();
+                    .Include(b => b.Guest)
+                    .Include(b => b.Homestay)
+                    .Include(b => b.Room)
+                    .Where(b => b.GuestId == CurrentUser.Id) // Filter by the current user's ID
+                    .ToListAsync();
             }
         }
     }
